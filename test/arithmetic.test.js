27baa3793ed8f1fce9359a996ace9ff42bef429c
Shreekant Bharti <186001946+Shreekant-Bharti@users.noreply.github.com>
2025-12-17 17:53:04 +0000

    Added power function tests

--- test/arithmetic.test.js
+++ test/arithmetic.test.js
@@ -94,6 +94,48 @@ describe('Arithmetic', function () {
     });
 
 // TODO: Challenge #1
+// test the power function
+    describe('Power', function () {
+        it('raises a positive integer to a positive integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 8 });
+                    done();
+                });
+        });
+        it('raises a positive integer to the zero power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=5&operand2=0')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 1 });
+                    done();
+                });
+        });
+        it('raises a negative integer to an even positive integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=-2&operand2=4')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: 16 });
+                    done();
+                });
+        });
+        it('raises a negative integer to an odd positive integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=-2&operand2=3')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res.body).to.eql({ result: -8 });
+                    done();
+                });
+        });
+        it('raises a floating point number to a positive integer power', function (done) {
+            request.get('/arithmetic?operation=power&operand1=1.5&operand2=2')
+                .expect(200)
+                .end(function (err, res) { expect(res.body).to.eql({ result: 2.25 });
+                    done();
+                });
+        });
+});     
  
 
     describe('Multiplication', function () {
